cc52731a3e16de56acdfbea5cea3491aaf9005c8
Vagner de Oliveira Conhariki <vconhariki@gmail.com>
2024-06-29 20:46:58 -0300

    Adicionar arquivos de projeto.

---
+++ PdfHelper/PdfService.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace PdfGenerator.Service
+{
+    public class PdfService
+    {
+
+        private static readonly HttpClient client = new();
+
+        public static async Task<string> HtmlToBase64(string html, string url)
+        {
+            var requestContent = new MultipartFormDataContent();
+            var stringContent = new StringContent(html, Encoding.UTF8, "text/html");
+
+            requestContent.Add(stringContent, "file", "index.html");
+
+            var response = await client.PostAsync(url, requestContent);
+
+            if (response.IsSuccessStatusCode && response.Content != null)
+            {
+                var byteArray = await response.Content.ReadAsByteArrayAsync();
+                var base64 = Convert.ToBase64String(byteArray);
+                return base64;
+            }
+
+            return string.Empty;
+        }
+
+        public static async Task<string> LinkToBase64(string link, string url) 
+        {
+            var requestContent = new MultipartFormDataContent
+            {
+                { new StringContent(link), "url" },
+                { new StringContent("PDF/A-1a"), "pdfFormat" }
+            };
+
+            var response = await client.PostAsync(url, requestContent);
+
+            if (response.IsSuccessStatusCode && response.Content != null)
+            {
+                var byteArray = await response.Content.ReadAsByteArrayAsync();
+                var base64 = Convert.ToBase64String(byteArray);
+                return base64;
+            }
+
+            return string.Empty; 
+        }
+    }
+}
