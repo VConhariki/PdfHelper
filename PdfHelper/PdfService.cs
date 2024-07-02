2d6fc6427ac950dd57e5c188dfbae12a64d157b0
Vagner de Oliveira Conhariki <vconhariki@gmail.com>
2024-07-02 09:58:56 -0300

    - new feature returning memomrystream  - code cleaning

--- PdfHelper/PdfService.cs
+++ PdfHelper/PdfService.cs
@@ -9,6 +9,35 @@ namespace PdfGenerator.Service
 
         public static async Task<string> HtmlToBase64(string html, string url)
         {
+            var bytes = await GetBytesFromHtml(html, url);
+            var base64 = GetBase64FromBytes(bytes);
+            return base64;
+        }
+
+        public static async Task<MemoryStream> HtmlToMemoryStream(string html, string url)
+        {
+            var bytes = await GetBytesFromHtml(html, url);
+            var memoryStream = GetMemoryStreamFromBytes(bytes);
+            return memoryStream;
+        }
+
+        public static async Task<string> LinkToBase64(string link, string url)
+        {
+            var bytes = await GetBytesFromLink(link, url);
+            var base64 = GetBase64FromBytes(bytes);
+            return base64;
+        }
+
+        public static async Task<MemoryStream> LinkToMemoryStrem(string link, string url)
+        {
+            var bytes = await GetBytesFromLink(link, url);
+            var memoryStream = GetMemoryStreamFromBytes(bytes);
+            return memoryStream;
+        }
+
+        #region private
+        private static async Task<byte[]> GetBytesFromHtml(string html, string url)
+        {
             var requestContent = new MultipartFormDataContent();
             var stringContent = new StringContent(html, Encoding.UTF8, "text/html");
 
@@ -19,14 +48,12 @@ namespace PdfGenerator.Service
             if (response.IsSuccessStatusCode && response.Content != null)
             {
                 var byteArray = await response.Content.ReadAsByteArrayAsync();
-                var base64 = Convert.ToBase64String(byteArray);
-                return base64;
+                return byteArray;
             }
-
-            return string.Empty;
+            return [];
         }
 
-        public static async Task<string> LinkToBase64(string link, string url) 
+        private static async Task<byte[]> GetBytesFromLink(string link, string url)
         {
             var requestContent = new MultipartFormDataContent
             {
@@ -39,11 +66,32 @@ namespace PdfGenerator.Service
             if (response.IsSuccessStatusCode && response.Content != null)
             {
                 var byteArray = await response.Content.ReadAsByteArrayAsync();
-                var base64 = Convert.ToBase64String(byteArray);
+                return byteArray;
+            }
+            return [];
+        }
+
+        private static MemoryStream GetMemoryStreamFromBytes(byte[] bytes)
+        {
+            if (bytes.Length > 0)
+            {
+                var memoryStream = new MemoryStream(bytes);
+                return memoryStream;
+            }
+
+            return new MemoryStream();
+        }
+
+        private static string GetBase64FromBytes(byte[] bytes)
+        {
+            if (bytes.Length > 0)
+            {
+                var base64 = Convert.ToBase64String(bytes);
                 return base64;
             }
 
-            return string.Empty; 
+            return string.Empty;
         }
+        #endregion
     }
 }
